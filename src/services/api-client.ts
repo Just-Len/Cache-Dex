0aabafa532b12bca94a7b89e6cb8cc011b9cb149
CocoCR300 <55771921+CocoCR300@users.noreply.github.com>
2025-04-03 16:42:36 -0600

    Deduplicate code

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -19,75 +19,46 @@ interface IndexItem
 
 export class ApiClient
 {
-	// Name: names[index].name
-	async moves(count = 64, offset = 0): Promise<PokeAPI.Move[]>
+	private async fetchFromIndex<T>(endpoint: String, count: number, offset: number): Promise<T[]>
 	{
-		const indexResponse = await fetch(`${API_URL}/move?limit=${count}&offset=${offset}`);
+		const indexResponse = await fetch(`${API_URL}/${endpoint}?limit=${count}&offset=${offset}`);
 		if (!indexResponse.ok) {
 			throw new Error("Couldn't fetch moves");
 		}
 
 		const index: PaginatedResponse<IndexItem> = await indexResponse.json();
-		const movePromises = index.results.map(item => fetch(item.url as any));
-		const moveResponses = await Promise.all(movePromises);
-		const moves: PokeAPI.Move[] = await Promise.all(
-			moveResponses.filter(response => response.ok).map(response => response.json())
+		const itemPromises = index.results.map(item => fetch(item.url as any));
+		const itemResponses = await Promise.all(itemPromises);
+		const items: T[] = await Promise.all(
+			itemResponses.filter(response => response.ok).map(response => response.json())
 		);
 
-		return moves;
+		return items;
+	}
+
+	// Name: names[index].name
+	async moves(count = 64, offset = 0): Promise<PokeAPI.Move[]>
+	{
+		return this.fetchFromIndex("move", count, offset);
 	}
 
 	// Localized name is in PokemonSpecies, not here
 	// Image: sprites.other["official-artwork"].front_default
 	async pokemon(count = 64, offset = 0): Promise<PokeAPI.Pokemon[]>
 	{
-		const indexResponse = await fetch(`${API_URL}/pokemon?limit=${count}&offset=${offset}`);
-		if (!indexResponse.ok) {
-			throw new Error("Couldn't fetch pokémon");
-		}
-
-		const index: PaginatedResponse<IndexItem> = await indexResponse.json();
-		const pokemonPromises = index.results.map(item => fetch(item.url as any));
-		const pokemonResponses = await Promise.all(pokemonPromises);
-		const pokemons: PokeAPI.Pokemon[] = await Promise.all(
-			pokemonResponses.filter(response => response.ok).map(response => response.json())
-		);
-
-		return pokemons;
+		return this.fetchFromIndex("pokemon", count, offset);
 	}
 
 	// Name: names[index].name
 	async pokemonSpecies(count = 64, offset = 0): Promise<PokeAPI.PokemonSpecies[]>
 	{
-		const indexResponse = await fetch(`${API_URL}/pokemon-species?limit=${count}&offset=${offset}`);
-		if (!indexResponse.ok) {
-			throw new Error("Couldn't fetch pokémon species");
-		}
-
-		const index: PaginatedResponse<IndexItem> = await indexResponse.json();
-		const speciesPromises = index.results.map(item => fetch(item.url as any));
-		const speciesResponses = await Promise.all(speciesPromises);
-		const species: PokeAPI.PokemonSpecies[] = await Promise.all(
-			speciesResponses.filter(response => response.ok).map(response => response.json())
-		);
-		return species;
+		return this.fetchFromIndex("pokemon-species", count, offset);
 	}
 
 	// Name: names[index].name
 	async pokemonTypes(count = 64, offset = 0): Promise<PokeAPI.PokemonType[]>
 	{
-		const indexResponse = await fetch(`${API_URL}/type?limit=${count}&offset=${offset}`);
-		if (!indexResponse.ok) {
-			throw new Error("Couldn't fetch pokémon types");
-		}
-
-		const index: PaginatedResponse<IndexItem> = await indexResponse.json();
-		const typePromises = index.results.map(item => fetch(item.url as any));
-		const typeResponses = await Promise.all(typePromises);
-		const types: PokeAPI.PokemonType[] = await Promise.all(
-			typeResponses.filter(response => response.ok).map(response => response.json())
-		);
-		return types;
+		return this.fetchFromIndex("type", count, offset);
 	}
 }
 
